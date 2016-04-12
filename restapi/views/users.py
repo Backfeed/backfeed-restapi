1ebde27c2dc3f29a345a6bebc38baff87c2d45db
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-12 17:52:13 +0200

    implement user CRUD

--- restapi/views/users.py
+++ restapi/views/users.py
@@ -1,46 +1,61 @@
-from cornice import Service
-
+from cornice.resource import resource, view
 import protocol
 
-users_service = Service(name='users', path='/{contract}/users', description="Users Collection")
-user_service = Service(name='user', path='/{contract}/users/{user_id}', description="User")
 
+@resource(collection_path='/{contract}/users', path='/{contract}/users/{id}')
+class User(object):
 
-@users_service.get()
-def get_users(request):
-    """Returns a list of users"""
-    # get the contract
-    contract_name = request.matchdict['contract']
-    contract = protocol.get_contract(contract_name)
+    def __init__(self, request):
+        self.request = request
+        contract_name = self.request.matchdict['contract']
+        self.contract = protocol.get_contract(contract_name)
 
-    users = contract.get_users()
-    return {
-        'count': len(users),
-        'items': [user for user in users],
-    }
+    def collection_get(self):
+        """Get a list of users"""
+        users = self.contract.get_users()
+        return {
+            'count': len(users),
+            'items': [self.user_to_dict(user) for user in users],
+        }
 
+    @view(renderer='json')
+    def collection_post(self):
+        """Create a *new user*
 
-@user_service.get()
-def get_user(request):
-    """return the user identified by `user_id`
-    """
-    user_id = request.matchdict['user_id']
-    contract_name = request.matchdict['contract']
-    contract = protocol.get_contract(contract_name)
-    user = contract.get_user(user_id)
-    return {
-        'id': user.id,
-    }
+        :param tokens: the amount of tokens to assign to this user. Must be an integer or a float (like 3.14)
 
+        :param reputation: the amount of reputation to assign to this user. Must be an integer or a float (like 3.14)
 
-@user_service.post()
-def update_user(request):
-    """update the user information"""
-    user_id = request.matchdict['user_id']
+        user
+        """
+        user = self.contract.create_user(**self.request.POST)
+        return self.user_to_dict(user)
 
-    contract_name = request.matchdict['contract']
-    contract = protocol.get_contract(contract_name)
-    user = contract.get_user(user_id)
-    return {
-        'id': user.id,
-    }
+    @view(renderer='json')
+    def get(self):
+        """Get the user"""
+        user_id = self.request.matchdict['id']
+        user = self.contract.get_user(user_id)
+        return self.user_to_dict(user)
+
+    @view(renderer='json')
+    def put(self):
+        """update information of this user"""
+        user_id = self.request.matchdict['id']
+        user = self.contract.update_user(user_id=user_id, **self.request.POST)
+        return self.user_to_dict(user)
+
+    @view()
+    def delete(self):
+        """Delete this user"""
+        user_id = self.request.matchdict['id']
+        self.contract.delete_user(user_id)
+
+    @staticmethod
+    def user_to_dict(user):
+        """return a dictionary with information about this user"""
+        return {
+            'id': user.id,
+            'tokens': float(user.tokens),
+            'repuation': float(user.reputation),
+        }
