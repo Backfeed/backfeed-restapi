ce9391552d2667c28dc5b3ba0da21d087e2d82c6
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-16 14:39:37 +0200

    init db on startup

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -6,8 +6,13 @@ __email__ = 'jelle@gerbrandy.com'
 __version__ = '0.1'
 
 
+
 def main(global_config, **settings):
     from pyramid.config import Configurator
+
+    from backfeed_protocol import utils
+    utils.init_database('/tmp/backfeed.db')
+    
     config = Configurator(settings=settings)
     config.include("cornice")
     config.scan("restapi.views")
