4729c11fd1df119bcff282a67d71411cbcb2e675
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-17 11:42:57 +0200

    fix tox

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -6,14 +6,16 @@ __email__ = 'jelle@gerbrandy.com'
 __version__ = '0.1'
 
 
-
 def main(global_config, **settings):
-    from pyramid.config import Configurator
-
+    import pyramid
     from backfeed_protocol import utils
-    utils.init_database('/tmp/backfeed.db')
-    
-    config = Configurator(settings=settings)
+    # define the database connection
+    sqlite_db = settings.get('sqlite_db')
+    if not sqlite_db:
+        raise Exception('please specify the sqlite_db setting')
+    utils.init_database(sqlite_db)
+
+    config = pyramid.config.Configurator(settings=settings)
     config.include("cornice")
     config.scan("restapi.views")
     config.scan("restapi.views.contributions")
--- restapi/tests/common.py
+++ restapi/tests/common.py
@@ -8,8 +8,9 @@ from restapi import main
 class APITestCase(unittest.TestCase):
     """Base class for testing API functions"""
     def setUp(self):
-        protocol.setup_database(':memory:')
-        self.app = TestApp(main({}))
+        sqlite_db = ':memory:'
+        protocol.setup_database(sqlite_db)
+        self.app = TestApp(main({}, sqlite_db=sqlite_db))
         self.contract_name = 'contract1'
         self.contract = protocol.get_contract(self.contract_name)
         self.url_users_collection = '/{contract}/users'.format(contract=self.contract_name)
