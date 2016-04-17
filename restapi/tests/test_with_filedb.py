3dd081eca31313d5243980039f9962bcbbd44a4e
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-17 21:47:10 +0200

    add tests for working with file database

--- restapi/tests/test_with_filedb.py
+++ restapi/tests/test_with_filedb.py
@@ -1,9 +1,16 @@
+import os
+
 from common import APITestCase
 
 
 class TestWithDBFile(APITestCase):
     """All the other tests run in memory - here we test with files"""
     sqlite_db = '/tmp/backfeed_test.db'
+
+    def setUp(self):
+        if os.path.exists(self.sqlite_db):
+            os.remove(self.sqlite_db)
+        super(TestWithDBFile, self).setUp()
 
     def test_sanity(self):
         # the connection is made and defined in self.setUp
