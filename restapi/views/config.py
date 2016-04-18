71750170ae0e42eba195b23d265ec06d6bf18dee
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-18 14:55:51 +0200

    add config.py to the repo

---
+++ restapi/views/config.py
@@ -0,0 +1,7 @@
+
+URL_CONTRIBUTION_COLLECTION = '/{contract}/contributions'
+URL_CONTRIBUTION_RESOURCE = '/{contract}/contributions/{id}'
+URL_EVALUATION_COLLECTION = '/{contract}/evaluations'
+URL_EVALUATION_RESOURCE = '/{contract}/evaluations/{id}'
+URL_USER_COLLECTION = '/{contract}/users'
+URL_USER_RESOURCE = '/{contract}/users/{id}'
