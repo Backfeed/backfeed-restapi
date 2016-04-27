1cf8df2eebae4f0e7538c5f2dc7e8e476bbc877f
Jelle Gerbrandy <github@gerbrandy.com>
2016-04-27 11:35:06 +0200

    add an access point to get some info

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -31,23 +31,6 @@ def main(global_config, **settings):
     utils.setup_database(settings=settings)
     config.registry.dbmaker = sessionmaker(bind=engine)
     config.add_request_method(db, reify=True)
-#     # define the database connection
-#     sqlite_db = settings.get('sqlite_db')
-#     if not sqlite_db:
-#         raise Exception('please specify the sqlite_db setting')
-#     # utils.init_database(sqlite_db)
-#     database = utils.init_database(sqlite_db)
-
-#     if sqlite_db != ':memory:':
-#         # create the database file if it does not exists
-#         if not os.path.exists(sqlite_db):
-#             utils.setup_database(sqlite_db)
-#         try:
-#             database.connect()
-#         except Exception as error:
-#             msg = 'Error connecting to {sqlite_db}'.format(sqlite_db=sqlite_db)
-#             msg += unicode(error)
-#             raise Exception(msg)
 
     config = Configurator(settings=settings)
     config.include("cornice")
